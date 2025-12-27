35b92f356c3d8f1e7723e9e5e9667af018124fd0
yairf12q <ticnutai1@gmail.com>
2025-12-27 18:30:32 +0000

    Fix overflow-x hidden causing unwanted arrow cursors

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.169qvbrne8g"
+    "revision": "0.37u1o6vfsmc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
